7478e4a357a22976a6fe72abd4b3468cb57ebde4
Casey Gwyn Dable <cgdable@up.edu.ph>
2021-07-16 13:26:23 +0800

    App.js edit

---
+++ src/App/components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <title className="App-title">
+        Snippets.io
+      </title>
+      <title className="App-subtitle">
+        hakdgoooo
+      </title>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/components/Login.js
@@ -0,0 +1,12 @@
+import './Login.css';
+
+function Login() {
+    return (
+
+        <div>
+            <header>hakdog</header>
+        </div>
+    );
+}
+
+export default Login;
