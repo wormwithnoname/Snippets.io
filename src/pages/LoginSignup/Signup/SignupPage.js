edc7cdc8f6b7c7d47369dd0451ca015d076a18e7
Casey Gwyn Dable <cgdable@up.edu.ph>
2021-07-26 10:40:53 +0800

    [misc][case] changed text to typography antd components

--- src/pages/LoginSignup/Signup/SignupPage.js
+++ src/pages/LoginSignup/Signup/SignupPage.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import { Typography } from 'antd';
+
 import SignupCard from 'components/SignupCard/SignupCard';
 
 import Logo from 'assets/img/logowhite.svg';
 
 import './SignupPage.scss';
 
+const { Title } = Typography;
+
 function SignupPage() {
   return (
     <div className="signup-page">
       <img src={Logo} alt="snippets logo" />
-      <p className="signup-subtitle">
+      <Title level={4} className="signup-subtitle">
         An organized, searchable, shareable, and simple code snippets clipboard
-      </p>
+      </Title>
       <SignupCard />
     </div>
   );
