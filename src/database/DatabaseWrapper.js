26758ca3b58eadca681957ddcb635047f7c92d51
jamescasia <jamescasia99@gmail.com>
2021-07-30 11:02:05 +0800

    [adjustment][gabrie] refactored fetch queries

--- src/database/DatabaseWrapper.js
+++ src/database/DatabaseWrapper.js
@@ -10,34 +10,44 @@ import {
   fetchSnippets,
 } from './queries/fetch';
 
-export const getAllSnippets = async () => fetchSnippets();
-export const getAllUsers = async () => fetchUsers();
+export const getAllSnippets = async () => {
+  const snippets = fetchSnippets();
+  console.log(snippets);
+};
+export const getAllUsers = async () => {
+  const users = fetchUsers();
+  console.log(users);
+};
 
 export const getOwnedSnippets = async (uid) => {
   const snippetIDs = await fetchOwnedSnippetIDs(uid);
   const ownedSnippets = await fetchOwnedSnippets(snippetIDs);
+  console.log(ownedSnippets);
+
   return ownedSnippets;
 };
 
 export const getViewSnippets = async (uid) => {
   const snippetIDs = await fetchViewSnippetIDs(uid);
-  const ownedSnippets = await fetchViewSnippets(snippetIDs);
-  return ownedSnippets;
+  const viewSnippets = await fetchViewSnippets(snippetIDs);
+  console.log(viewSnippets);
+  return viewSnippets;
 };
 export const getEditSnippets = async (uid) => {
   const snippetIDs = await fetchEditSnippetIDs(uid);
-  const ownedSnippets = await fetchEditSnippets(snippetIDs);
-  return ownedSnippets;
+  const editSnippets = await fetchEditSnippets(snippetIDs);
+  console.log(editSnippets);
+  return editSnippets;
 };
 
 export const getUsers = async () => {
-  console.log('lol');
-  const res = await fetchUsers();
-  return res;
+  const users = await fetchUsers();
+  console.log(users);
+  return users;
 };
 
 export const getUserInfo = async (uid) => {
-  console.log('lol');
-  const res = await fetchUserInfo(uid);
-  return res;
+  const userInfo = await fetchUserInfo(uid);
+  console.log(userInfo);
+  return userInfo;
 };
--- src/database/queries/fetch.js
+++ src/database/queries/fetch.js
@@ -1,83 +1,39 @@
 import { db } from '../../services/FirebaseService';
 import { Snippet } from '../../classes/Snippet';
-import { User } from '../../classes/User';
+// import { User } from '../../classes/User';
 import { UserInfo } from '../../classes/UserInfo';
 
-const users = db.collection('users-test');
-const snippets = db.collection('snippets-test');
-const userInfo = db.collection('user-info-test');
+const Users = db.collection('user-test');
+const Snippets = db.collection('snippets-test');
+const UserInfos = db.collection('user-info-test');
 
 // Admin fetch all
-export const fetchUsers = async () =>
-  users
-    .get()
-    .then()
-    .map((user) => User.from(user));
+export const fetchUsers = async () => Users.get();
+// Users.get().then((users) => users.map((u) => User.from(u)));
 
-export const fetchSnippets = async () =>
-  snippets
-    .get()
-    .then()
-    .map((snip) => Snippet.from(snip));
+export const fetchSnippets = async () => Snippets.get();
 
 // User Data-related
-export const fetchOwnedSnippetIDs = async (uid) => {
-  const ownedSnippetIDs = await (
-    await db.collection('user-test').doc(uid).get()
-  ).get('ownedSnippetIDs');
-  console.log('snippetids');
-  console.log(ownedSnippetIDs);
-  return ownedSnippetIDs;
-};
+export const fetchOwnedSnippetIDs = async (uid) =>
+  (await Users.doc(uid).get()).get('ownedSnippetIDs');
 
-export const fetchOwnedSnippets = async (snippetIDs) => {
-  const ownedSnippets = snippetIDs.map(async (sID) => {
-    const snippet = Snippet.from((await snippets.doc(sID).get()).data());
-    return snippet;
-  });
-  console.log(ownedSnippets);
-  return ownedSnippets;
-};
+export const fetchOwnedSnippets = async (snippetIDs) =>
+  snippetIDs.map(async (sID) => Snippet.from((await Snippets.doc(sID).get()).data()));
 
-export const fetchEditSnippetIDs = async (uid) => {
-  const editSnippetIDs = await (
-    await db.collection('user-test').doc(uid).get()
-  ).get('editSnippetIDs');
-  console.log('snippetids');
-  console.log(editSnippetIDs);
-  return editSnippetIDs;
-};
+export const fetchEditSnippetIDs = async (uid) =>
+  (await Users.doc(uid).get()).get('editSnippetIDs');
 
-export const fetchEditSnippets = async (snippetIDs) => {
-  const editSnippets = snippetIDs.map(async (sID) => {
-    const snippet = Snippet.from((await snippets.doc(sID).get()).data());
-    return snippet;
-  });
-  console.log(editSnippets);
-  return editSnippets;
-};
+export const fetchEditSnippets = async (snippetIDs) =>
+  snippetIDs.map(async (sID) => Snippet.from((await Snippets.doc(sID).get()).data()));
 
-export const fetchViewSnippetIDs = async (uid) => {
-  const viewSnippetIDs = await (
-    await db.collection('user-test').doc(uid).get()
-  ).get('viewSnippetIDs');
-  console.log('snippetids');
-  console.log(viewSnippetIDs);
-  return viewSnippetIDs;
-};
+export const fetchViewSnippetIDs = async (uid) =>
+  (await Users.doc(uid).get()).get('viewSnippetIDs');
 
-export const fetchViewSnippets = async (snippetIDs) => {
-  const viewSnippets = snippetIDs.map(async (sID) => {
-    const snippet = Snippet.from((await snippets.doc(sID).get()).data());
-    return snippet;
-  });
-  console.log(viewSnippets);
-  return viewSnippets;
-};
+export const fetchViewSnippets = async (snippetIDs) =>
+  snippetIDs.map(async (sID) => Snippet.from((await Snippets.doc(sID).get()).data()));
 
 // public
 export const fetchUserInfo = async (uid) => {
-  const data = (await userInfo.doc(uid).get()).data();
-  console.log(UserInfo.from(data));
+  const data = (await UserInfos.doc(uid).get()).data();
   return UserInfo.from(data);
 };
