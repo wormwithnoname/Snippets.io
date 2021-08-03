259592d4f4b7dd297d2d4eedee461a110ff1723d
Patricia Lexa Tumulak <tumulakpatricia@gmail.com>
2021-08-03 18:13:13 +0800

    [adjustment][pat] add language dropdown menu to snippet card

--- src/components/SnippetCard/index.js
+++ src/components/SnippetCard/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
 
-import { Card, Typography } from 'antd';
-import { EllipsisOutlined } from '@ant-design/icons';
+import { Button, Card, Dropdown, Menu, message, Typography } from 'antd';
+import { DownOutlined, EllipsisOutlined } from '@ant-design/icons';
+
+import CIcon from 'assets/img/c_icon.svg';
+import JavaScriptIcon from 'assets/img/javascript_icon.svg';
+import PythonIcon from 'assets/img/python_icon.svg';
 
 import './styles.scss';
 
-const { Text } = Typography;
+const { Link, Text } = Typography;
+
+function handleMenuClick() {
+  message.info('Click on menu item.');
+}
+
+const menu = (
+  <Menu className="menu-icon" onClick={handleMenuClick}>
+    <Menu.Item key="1" icon={<img alt="Python" src={PythonIcon} />}>
+      Python
+    </Menu.Item>
+    <Menu.Item key="2" icon={<img alt="JavaScript" src={JavaScriptIcon} />}>
+      JavaScript
+    </Menu.Item>
+    <Menu.Item key="3" icon={<img alt="C" src={CIcon} />}>
+      C++
+    </Menu.Item>
+  </Menu>
+);
 
 function SnippetCard() {
   return (
     <div className="snippet-container">
       <Card
         className="snippet-card"
-        extra={<EllipsisOutlined />}
-        hoverable="true"
+        extra={
+          <>
+            <Dropdown overlay={menu}>
+              <Button shape="round" className="extra-button">
+                Language <DownOutlined />
+              </Button>
+            </Dropdown>
+            <Link classname="snippet-ellipsis" href="/">
+              <EllipsisOutlined />
+            </Link>
+          </>
+        }
         title="Card Title"
       >
         <Text level={4}>content hereeee</Text>
