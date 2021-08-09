9b92dc9d846c7806c352fef614a28aa503707d6c
Casey Gwyn Dable <cgdable@up.edu.ph>
2021-08-09 17:27:48 +0800

    [feat][case] added view snippet modal

--- src/components/SnippetCard/index.js
+++ src/components/SnippetCard/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-import { Button, Card, message, Typography } from 'antd';
+import { Card, message, Modal, Typography } from 'antd';
 
-import CardDropdown from 'components/CardDropdown';
+// import CardDropdown from 'components/CardDropdown';
 
 import './styles.scss';
 
@@ -12,26 +12,47 @@ const { Text } = Typography;
 const tagNames = ['Frontend', 'Web'];
 
 const tags = tagNames.map((tagName) => (
-  <Button className="tagButton" shape="round">
-    {tagName}
-  </Button>
+  <div>
+    <Text> #{tagName}</Text>
+  </div>
 ));
 
 function SnippetCard() {
   function onCopyText() {
     message.success('Copied!');
   }
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const textInput = 'Test Data';
   // replace with the data input by the user
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <div className="snippet-container">
-      <Card actions={[tags]} className="snippet-card" extra={<CardDropdown />} title="Card Title">
-        <CopyToClipboard text={textInput} onCopy={onCopyText}>
-          <Text> {textInput} </Text>
-        </CopyToClipboard>
+      <Card onClick={showModal} actions={[tags]} className="snippet-card" title="Card Title">
+        <Text>{textInput}</Text>
       </Card>
+      <Modal
+        className="snippet-view"
+        title="This Snippet"
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <div className="snippet-container">
+          <CopyToClipboard text={textInput} onCopy={onCopyText}>
+            <Text> {textInput} </Text>
+          </CopyToClipboard>
+          <Text keyboard> Tags </Text>
+          {[tags]}
+        </div>
+      </Modal>
     </div>
   );
 }
