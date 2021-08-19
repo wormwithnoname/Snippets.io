b497781a258ced16bcfb171ebae56bce51c0c0af
aosuarez <aosuarez@up.edu.ph>
2021-08-19 13:22:29 +0800

    [misc][ana] added delete folder button and modal

--- src/components/Folder/index.jsx
+++ src/components/Folder/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { Typography } from 'antd';
-import { FolderOutlined } from '@ant-design/icons';
+import { Card, Divider, Modal, Row, Typography } from 'antd';
+import { DeleteOutlined, FolderOutlined } from '@ant-design/icons';
 
 import './styles.scss';
 import routes from 'constants/routes';
@@ -10,15 +10,46 @@ import routes from 'constants/routes';
 const { Text } = Typography;
 
 function Folder({ folderObj }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const history = useHistory();
 
   async function redirectPage() {
     history.push(routes.FOLDER, folderObj);
   }
+
+  function showModal() {
+    setIsModalVisible(true);
+  }
+
+  function handleOk() {
+    setIsModalVisible(false);
+  }
+
+  function handleCancel() {
+    setIsModalVisible(false);
+  }
+
   return (
     <div className="folder">
-      <FolderOutlined className="folderIcon" onClick={redirectPage} />
-      <Text className="folderName">{folderObj.folderName}</Text>
+      <Card bordered={false}>
+        <div className="folder-body">
+          <FolderOutlined className="folderIcon" onClick={redirectPage} />
+          <Text className="folderName">{folderObj.folderName}</Text>
+        </div>
+        <Divider />
+        <Row>
+          <DeleteOutlined onClick={showModal} />
+        </Row>
+      </Card>
+      <Modal
+        okText="Delete"
+        onOk={handleOk}
+        title="Delete folder"
+        onCancel={handleCancel}
+        visible={isModalVisible}
+      >
+        <p>Are you sure you want to delete this folder?</p>
+      </Modal>
     </div>
   );
 }
